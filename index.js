a1e595d9203c5a7c2aff97fc31ee8e44f567878d
Zaid Saiyed <zaid.akil.saiyed@gmail.com>
2023-06-11 00:24:08 -0400

    Organizing Mongo Stuff and creating User Model Schema

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mogoURI, { useUnifiedTopology: true });  // Connect to the MongoDB
 
 const app = express();
-
 
 app.get("/", (req, res) => {
 	res.send({ message: "This is kind of test for node-dev module" });
@@ -9,3 +12,4 @@ app.get("/", (req, res) => {
 
 console.log("Hello World Zaid");
 app.listen(2222, () => console.log("Server running on port 2222"));
+
