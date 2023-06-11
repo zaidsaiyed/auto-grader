c0cdfe78cb0b7ffffe13a4ddb0432b1fe1a6ace0
Zaid Saiyed <zaid.akil.saiyed@gmail.com>
2023-06-11 00:46:15 -0400

    Created Routes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 
-mongoose.connect(keys.mogoURI, { useUnifiedTopology: true });  // Connect to the MongoDB
+mongoose.connect(keys.mongoURI, { useUnifiedTopology: true });  // Connect to the MongoDB
 
 const app = express();
 
@@ -12,4 +12,3 @@ app.get("/", (req, res) => {
 
 console.log("Hello World Zaid");
 app.listen(2222, () => console.log("Server running on port 2222"));
-
