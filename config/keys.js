a91c8472044cbe71d68c5a93ff183edc36e2e50e
Zaid Saiyed <zaid.akil.saiyed@gmail.com>
2023-06-11 00:31:36 -0400

    Configured Mongo and created new User Model Schema

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    "mogoURI": "mongodb+srv://saiy5983:Pioneer450@autograder.rrj7uwp.mongodb.net/",
+};
---
+++ models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+
+const userSchema = new Schema({
+    user_name: String,
+    student_id: String,
+    password: String,
+    types: {
+        type: String,
+        enum: ['A', 'B', ''],
+        default: 'A',
+      },
+}, {
+    timestamps: true
+});
+
+mongoose.model('users', userSchema);
