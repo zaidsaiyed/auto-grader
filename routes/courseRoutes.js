9a81004402462a64f26f08a178d05052fd202ee0
Priyanshu Shah <105989704+MaDScienTisT21@users.noreply.github.com>
2023-06-13 18:14:46 -0400

    Updated courseRoutes.js, added error messages

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -2,33 +2,37 @@ const mongoose = require("mongoose");
 const Course = mongoose.model("course");
 
 module.exports = (app) => {
-    // Get course by id
+  // Get course by ID
   app.get("/api/course/:id", async (req, res) => {
+    const { id } = req.params;
+
     try {
-      if(req.params.id){
-        const courses = await Course.find({course_id : req.params.id}).exec();
-        res.json(courses);
+      const course = await Course.findOne({ course_id: id }).exec();
+      if (course) {
+        res.json(course);
+      } else {
+        res.status(404).json({ message: "Course not found" });
       }
-
-
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
   });
 
+  // Create a new course
   app.post("/api/course", async (req, res) => {
-    const{
+    const { course_id, course_name, prof } = req.body;
+
+    try {
+      const course = new Course({
         course_id,
         course_name,
-        prof
-    } = req.body;
+        prof,
+      });
 
-    const course = new Course({
-      course_id,
-      course_name,
-      prof
-    }).save();
-
-    res.send(course);
+      await course.save();
+      res.send(course);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
   });
 };
