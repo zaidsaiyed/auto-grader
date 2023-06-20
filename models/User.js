ac8133987e8aea570cb17119d2ff5b164d63cf4c
Priyanshu Shah <105989704+MaDScienTisT21@users.noreply.github.com>
2023-06-20 13:47:01 -0400

    Added Error throw for Duplicate user

--- models/User.js
+++ models/User.js
@@ -1,42 +1,48 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-
 const userSchema = new Schema({
-    user_name: {
-        type: String,
-        required: true,
-    },
-    student_id: {
-        type: Number,
-        required: true,
-        validate: {
-          validator: function (value) {
-            // Allow multiple documents with student_id = 0
-            if (value === 0) {
+  user_name: {
+    type: String,
+    required: true,
+  },
+  student_id: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (value) {
+        // Allow multiple documents with student_id = '0'
+        if (value === '0') {
+          return true;
+        }
+
+        // Check for uniqueness of student_id
+        return mongoose.models.user
+          .countDocuments({ student_id: value })
+          .then((count) => {
+            if (count === 0) {
               return true;
+            } else {
+              throw new Error('Duplicate student ID');
             }
-    
-            // Check for uniqueness of student_id
-            return mongoose.models.user.countDocuments({ student_id: value })
-              .then((count) => count === 0);
-          },
-          message: 'Student ID must be unique except when it is 0.',
-        },
-        maxlength: 9,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    types: {
-        type: String,
-        enum: ['A','S','P','TA'],
-        default: 'S',
-        required: true,
+          });
       },
+      message: 'Student ID must be unique except when it is \'0\'.',
+    },
+    maxlength: 9,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  types: {
+    type: String,
+    enum: ['A', 'S', 'P', 'TA'],
+    default: 'S',
+    required: true,
+  },
 }, {
-    timestamps: true
+  timestamps: true
 });
 
-mongoose.model('user', userSchema);
+module.exports = mongoose.model('user', userSchema);
