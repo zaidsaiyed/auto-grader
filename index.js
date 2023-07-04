5b78e2e43e8d034ee210e80c723f954ceed53d56
Zaid Saiyed <32754301+zaidsaiyed@users.noreply.github.com>
2023-07-03 23:44:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -119,6 +119,6 @@ app.get("/api/redirect", (req, res) => {
 	res.sendFile(__dirname + "/redirect.html");
 });
 
-console.log("\n**********************************************************");
+console.log("\n***********************************************************");
 
 app.listen(2222, () => console.log("Server running on port 2222. Visit http://localhost:2222/"));
