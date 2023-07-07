bcae49b120d7559b7ec93f72fbb21262d7f98277
Zaid Saiyed <zaid.akil.saiyed@gmail.com>
2023-07-07 04:03:43 -0400

    Testing unit tests

---
+++ Test_trials/test.js
@@ -0,0 +1,16 @@
+const { exec } = require('child_process');
+
+// Define the path to the Python script
+const pythonScriptPath = 'Test_trials/test.py';
+
+// Execute the Python script as a child process
+exec(`python ${pythonScriptPath}`, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error executing Python script: ${error}`);
+    //console.error(`Error executing Python script: Failed`);
+    return;
+  }
+
+  // Process the output from the Python script
+  console.log(`Python script output: ${stdout}`);
+});
