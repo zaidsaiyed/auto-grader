5cfc448111a5cff7fee1f7ea92e09abf0c5adc69
Zaid Saiyed <zaid.akil.saiyed@gmail.com>
2023-07-11 21:42:34 -0400

    generated custom outputs for test results

--- Test_trials/test.py
+++ Test_trials/test.py
@@ -32,3 +32,56 @@ class TestSum(unittest.TestCase):
 # run the tests
 if __name__ == '__main__':
     unittest.main()
+
+class CustomTestRunner(unittest.TextTestRunner):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Additional initialization if needed
+
+    def run(self, test):
+        # Customize the test result output
+        result = self._makeResult()
+        self.stream.writeln("Custom Test Runner - Starting the tests")
+        test(result)
+        self.stream.writeln("Custom Test Runner - Finished the tests")
+        self.stream.writeln("Custom Test Runner - Results:")
+        self.stream.writeln(result.separator2)
+        
+        # test1 = test._testMethodName
+        # print("this is test:", test1)
+        # print("------------------------------------------")
+        # print(result.errors)
+        # print("Done")
+        
+        # for test_case in test._tests:
+        #     test_name = test_case.id().split(".")[-1]
+        #     self.stream.writeln(f"Test: {test_name}")
+        #     self.stream.writeln("OK")
+        #     self.stream.writeln("")
+
+        for test_case, failure in result.failures:
+            test_name = test_case.id().split(".")[-1]
+            self.stream.writeln(f"Test: {test_name}")
+            self.stream.writeln("FAILED")
+            self.stream.writeln("")
+
+        for test_case, error in result.errors:
+            test_name = test_case.id().split(".")[-1]
+            self.stream.writeln(f"Test: {test_name}")
+            self.stream.writeln("ERROR")
+            self.stream.writeln("")
+
+        passed_tests = result.testsRun - len(result.failures) - len(result.errors)
+        total_tests = result.testsRun
+        self.stream.writeln(f"Final marks: {passed_tests} / {total_tests}")
+
+        return result
+
+
+
+# Create a test suite
+test_suite = unittest.TestLoader().loadTestsFromTestCase(TestSum)
+
+# Create a custom test runner and run the tests
+custom_runner = CustomTestRunner()
+custom_runner.run(test_suite)
